7b2b14e6a18e6550c1f2cac464819fdaf0550fcd
Manuel Serrano <Manuel.Serrano@uclm.es>
2024-04-08 22:42:39 +0200

    FizzBuzz

---
+++ FizzBuzz.hs
@@ -0,0 +1,14 @@
+import Data.Function -- Para importar los operadores (& y $)
+
+fizzBuzz :: Int -> String
+fizzBuzz x  
+   | (mod x 15 == 0) = "FizzBuzz"  
+   | (mod x  3 == 0) = "Fizz"  
+   | (mod x  5 == 0) = "Buzz"  
+   | otherwise       = show x
+
+main :: IO ()
+main = do
+    [1..30]
+        & map fizzBuzz
+        & print
